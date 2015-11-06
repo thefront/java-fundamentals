06521fabec7130f8c0762001c8d33ea9d0b51817
Rich Tzeng <rtzeng@mail.ccsf.edu>
2015-11-05 20:02:26 -0800

    add more demos

---
+++ examples/SuperClass1.java
@@ -0,0 +1,11 @@
+// Code-Listing 10-5
+
+public class SuperClass1 {
+  /**
+      Constructor
+  */
+
+  public SuperClass1() {
+    System.out.println("This is the superclass constructor");
+  }
+}
