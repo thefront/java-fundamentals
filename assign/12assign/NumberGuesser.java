34eb73256721011292450e849d0a79fc38b0ccd6
Rich Tzeng <rtzeng@mail.ccsf.edu>
2015-11-08 21:41:11 -0800

    more examples and assign 12

---
+++ assign/12assign/NumberGuesser.java
@@ -0,0 +1,65 @@
+// Name: Richard Tzeng
+// Date: 11/08/2015
+// Description: This progarm contains the NumberGuesser class which will
+//              contain all the logic for guessing the number:
+//              Constructor, lower, higher, getCurrentGuess, and reset methods
+//
+
+public class NumberGuesser {
+
+  // assign instance variables
+  protected int lowerGuess;    // lowerGuess value changes after each answer
+  protected int upperGuess;    // upperGuess value changes after each answer
+  protected int origLower;     // variable to set the original values
+  protected int origUpper;     // variable to set the original values
+
+
+  /**
+      The Constructor method to assign the values to the instance variables
+  */
+
+  public NumberGuesser(int lowerBound, int upperBound) {
+    lowerGuess = lowerBound;
+    upperGuess = upperBound;
+    origLower  = lowerBound;
+    origUpper  = upperBound;
+  }
+
+
+  /**
+      The higher method sets the lowerGuess value.
+  */
+
+  public void higher() {
+    lowerGuess = getCurrentGuess();
+  }
+
+
+  /**
+      The lower method sets the upperGuess value.
+  */
+
+  public void lower() {
+    upperGuess = getCurrentGuess();
+  }
+
+
+  /**
+      The getCurrentGuess method returns the current mid point value.
+      @return the mid point value.
+  */
+
+  public int getCurrentGuess() {
+    return (lowerGuess + upperGuess) / 2;
+  }
+
+
+  /**
+      The reset method sets the lower/upper values back to the original ones
+  */
+
+  public void reset() {
+    lowerGuess = origLower;
+    upperGuess = origUpper;
+  }
+}
