fdcfc5464807df997612adc52d260c51392d5c8d
Rich Tzeng <rtzeng@mail.ccsf.edu>
2015-11-24 20:53:26 -0800

    update assign15 and assign14

--- assign/14assign/NumberGuessers/NumberGuesser.java
+++ assign/14assign/NumberGuessers/NumberGuesser.java
@@ -1,29 +1,39 @@
 
 public class NumberGuesser {
-	
+
 	protected int high;
 	protected int low;
-	
+
 	private int originalHigh;
 	private int originalLow;
-	
+
 	public NumberGuesser(int l, int h) {
 		low = originalLow = l;
 		high = originalHigh = h;
 	}
-	
+
 	public int getCurrentGuess() {
 		return (high + low) / 2;
 	}
-	
-	public void higher() {
+
+	public void higher() throws IllegalStateException {
 		low = getCurrentGuess() + 1;
+
+		if ((low - 1) >= high) {
+			throw new IllegalStateException("Hey you are cheating! We guessed your " +
+								" number already! Try again!");
+		}
 	}
-	
-	public void lower() {
+
+	public void lower() throws IllegalStateException {
 		high = getCurrentGuess() - 1;
+
+		if ((high + 1) <= low) {
+			throw new IllegalStateException("Hey you are cheating! We guessed your " +
+								" number already! Try again!");
+		}
 	}
-	
+
 	public void reset() {
 		low = originalLow;
 		high = originalHigh;
---
+++ assign/15assign/conway/NeverAliveCell.java
@@ -0,0 +1,23 @@
+
+public class NeverAliveCell extends AbstractCell {
+	
+	public NeverAliveCell(int r, int c, ConwayWorld w) {
+		super(r, c, w);
+	}
+	
+	public  AbstractCell cellForNextGeneration() {
+		return this;
+	}
+	
+	public boolean willBeAliveInNextGeneration() {
+		return false;
+	}
+
+	public char displayCharacter() {
+		return 'X';
+	}
+	
+	public boolean isAlive() {
+		return false;
+	}
+}
